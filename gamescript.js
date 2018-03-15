4fdaab93b5890eec7067eb162f9ce6c481d59691
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-03-15 17:17:06 +0100

    Implemented a lotttt of things, only the final calculation remains

--- gamescript.js
+++ gamescript.js
@@ -11,8 +11,10 @@ var rolled_dices;
 /**
  * It will store all the available result containers. If the user already has selected "Full house", it won't
  * be stored anymore. So the program will loop over it, but won't take care of this one.
+ * The unavailable IDs are stored in locked_result_ids
  */
 var available_result_ids;
+var locked_result_ids;
 
 /**
  * Number of faces of the dices used
@@ -29,6 +31,12 @@ var NUMBER_OF_FACES = 6;
 var NUMBER_OF_DICES = 5;
 
 /**
+ * It represents the number of throw in a turn
+ */
+var number_of_throws;
+var INITIAL_NUMBER_OF_THROWS = 3;
+
+/**
  * Called when the document is ready, it is a sort of main
  */
 $(document).ready(function() {
@@ -41,13 +49,10 @@ $(document).ready(function() {
  * Used to init the game
  */
 function init(){
-    // Initialize the variables
-    selected_dices = [];
-    rolled_dices = [];
-    for(var i = 0 ; i <= NUMBER_OF_FACES ; ++ i) {
-        selected_dices.push(0);
-        rolled_dices.push(0);
-    }
+    updateInformation("Cliquez sur lancer les dés pour démarrer la partie.");
+    number_of_throws = INITIAL_NUMBER_OF_THROWS;
+
+    resetDices();
 
     available_result_ids = [];
     available_result_ids.push("#dice-result-1");
@@ -63,11 +68,39 @@ function init(){
     available_result_ids.push("#large-straight-result");
     available_result_ids.push("#chance-result");
     available_result_ids.push("#yahtzee-result");
+    locked_result_ids = [];
+
+    for(var i = 0 ; i < available_result_ids.length ; ++ i){
+        // Add the hover interaction
+        $(available_result_ids[i]).hover(function () {
+            $(this).css("background-color", "#1abc9c");
+        }, function(){
+            $(this).css("background-color", "");
+        });
+
+        // Add the on click interaction
+        $(available_result_ids[i]).click(function () {
+            onScoreClick($(this));
+        });
+
+        $(available_result_ids[i]).css("background-color", "");
+    }
 
     // Add the clicks event
 
     $("#roll").click(function () {
-        onRollClick();
+
+        if(number_of_throws !== 0 && !(countDices(rolled_dices) === 0 && number_of_throws !== INITIAL_NUMBER_OF_THROWS)) {
+            number_of_throws--;
+            onRollClick();
+            if(number_of_throws !== 0) {
+                updateInformation("Il vous reste " + number_of_throws + " lancers. <br>" +
+                    "Vous pouvez finir votre tour à tout moment en sélectionnant un score. <br>" +
+                    "Seulement les dés dans \"dés lancés\" seront relancés.");
+            }else{
+                updateInformation("Vous n'avez plus de lancers. Sélectionnez un score pour finir votre tour.");
+            }
+        }
     });
 
     $("#rolled-dices").on("click", "img", function () {
@@ -77,6 +110,18 @@ function init(){
     $("#selected-dices").on("click", "img", function () {
         onSelectedDiceClick($(this));
     });
+}
+
+/**
+ * Used to reset the dices
+ */
+function resetDices(){
+    selected_dices = [];
+    rolled_dices = [];
+    for(var i = 0 ; i <= NUMBER_OF_FACES ; ++ i) {
+        selected_dices.push(0);
+        rolled_dices.push(0);
+    }
 }
 
 /**
@@ -157,6 +202,10 @@ function countDices(array){
     return counter;
 }
 
+//
+//  EVENTS
+//
+
 /**
  * Used to handle the roll click
  */
@@ -191,6 +240,83 @@ function onSelectedDiceClick(sender){
 }
 
 /**
+ * Used to handle a click in a score box (means that the player selects this as the score
+ * @param sender JQuery element sender
+ * @param id id of the sender (#blabla)
+ */
+function onScoreClick(sender){
+    // We grant the user to click only if he rolled the dices
+    if(countDices(selected_dices) + countDices(rolled_dices) !== 0) {
+        var id = "#" + sender.attr("id");
+
+        // We need to check if the sender is available
+        var available = false;
+        var index = 0;
+        while (!available && index < available_result_ids.length) {
+            if (available_result_ids[index] === id)
+                available = true;
+            else
+                index++;
+        }
+
+        // We need to handle the fact that if the player made a second yahtzee we need to put 100 extra score
+        if (!available && id === "#yahtzee-result" && getYahtzeeScore(getAllDices()) !== 0) {
+            // HERE THE USER MADE A SECOND YAHTZEE
+            // TODO
+        }
+
+        if (available) {
+            // Remove its is from the available IDs
+            // So, first get its index
+            index = 0;
+            var found = false;
+            while (!found && index < available_result_ids.length) {
+                if (available_result_ids[index] === id)
+                    found = true;
+                else
+                    index++;
+            }
+
+            // We remove it from the available list
+            available_result_ids.splice(index, 1);
+            // and add it to the locked list
+            locked_result_ids.push(id);
+
+            // Now we change its style
+            $(id).unbind('mouseenter mouseleave'); // it is actually hover
+            $(id).css("background-color", "#109177");
+
+        }
+
+
+        if (available_result_ids.length !== 0) {
+            // Now reset the dices if there are remaining boxes
+            number_of_throws = INITIAL_NUMBER_OF_THROWS;
+            resetDices();
+            showDices();
+            updateResults();
+            updateInformation("Vous pouvez lancer les dés quand vous voulez.");
+        } else {
+            // If there aren't remaining boxes, stop the game
+            // TODO: calculate final score
+            // -> SUM
+            // -> BONUS IF SUM > ???
+            // -> TOTAL SCORE
+            // Add total score in the history
+        }
+    }
+}
+
+/**
+ * It updates the information message
+ * @param info information message
+ */
+function updateInformation(info){
+    $("#information").empty();
+    $("#information").append(info);
+}
+
+/**
  * Used to update all the results in the result table
  */
 function updateResults(){
@@ -198,14 +324,7 @@ function updateResults(){
     var score;
 
     // We put all the dice in the same array
-    var all_dices = [];
-    // Init
-    for(i = 0 ; i <= NUMBER_OF_FACES; ++ i)
-        all_dices.push(0);
-    // Set
-    for(i = 1 ; i <= NUMBER_OF_FACES ; ++ i){
-        all_dices[i] += selected_dices[i] + rolled_dices[i];
-    }
+    var all_dices = getAllDices();
 
     for(i = 0 ; i < available_result_ids.length ; ++ i){
         var id = available_result_ids[i];
@@ -267,117 +386,129 @@ function updateResults(){
                 break;
         }
     }
+}
 
-    // Score calculation methods
-
-    /**
-     * Calculates the score of X dices of a kind.
-     * If there are x dices of a kind, the score is the sum of all dices
-     * @param x the number of similar dices
-     * @param dices the dices array
-     * @returns {number} the score
-     */
-    function getXOfAKindScore(x, dices){
-        var index = 1;
-        var score = 0;
-        var found = false;
-        while(!found && index <= NUMBER_OF_FACES){
-            score += dices[index] * index;
-            if(dices[index] >= x)
-                found = true;
-            else
-                index ++;
-        }
-        if(found)
-            return score;
-        else
-            return 0;
+function getAllDices(){
+    // We put all the dice in the same array
+    var all_dices = [];
+    // Init
+    for(i = 0 ; i <= NUMBER_OF_FACES; ++ i)
+        all_dices.push(0);
+    // Set
+    for(i = 1 ; i <= NUMBER_OF_FACES ; ++ i){
+        all_dices[i] += selected_dices[i] + rolled_dices[i];
     }
+    return all_dices;
+}
 
-    /**
-     * Calculates the score of a full house
-     * If there is a pair and three dices of a kind, the score is 25 otherwise, 0
-     * @param dices the dices array
-     * @returns {number} the score
-     */
-    function getFullHouseScore(dices){
-        var index = 1;
-        var pair_found = false;
-        var three_of_a_kind_found = false;
-        while(!(pair_found && three_of_a_kind_found) && index <= NUMBER_OF_FACES){
-            if(dices[index] === 2)
-                pair_found = true;
-            else if(dices[index] === 3)
-                three_of_a_kind_found = true;
+// Score calculation methods
+
+/**
+ * Calculates the score of X dices of a kind.
+ * If there are x dices of a kind, the score is the sum of all dices
+ * @param x the number of similar dices
+ * @param dices the dices array
+ * @returns {number} the score
+ */
+function getXOfAKindScore(x, dices){
+    var index = 1;
+    var score = 0;
+    var found = false;
+    while(!found && index <= NUMBER_OF_FACES){
+        score += dices[index] * index;
+        if(dices[index] >= x)
+            found = true;
+        else
             index ++;
-        }
-
-        if(pair_found && three_of_a_kind_found)
-            return 25;
-        else
-            return 0;
     }
-
-    /**
-     * Calculates a straight score
-     * If it detects a straight which its size is aimed_straight_length, then the score
-     * is 40 if it has a 5 dices straight, otherwise 30.
-     * @param aimed_straight_length aimed straight length
-     * @param dices the dices array
-     * @returns {number} the score
-     */
-    function getStraightScore(aimed_straight_length, dices){
-        var max_straight_length = 0;
-        var straight_length = 0;
-
-        for(var index = 1 ; index <= NUMBER_OF_FACES ; ++ index){
-            // There is an element, so we start the straight otherwise we reset it
-            if(dices[index] !== 0)
-                straight_length ++;
-            else
-                straight_length = 0;
-
-            if(straight_length > max_straight_length){
-                max_straight_length = straight_length;
-            }
-        }
-
-        if(max_straight_length < aimed_straight_length)
-            return 0;
-        else if(aimed_straight_length === 4)
-            return 30;
-        else
-            return 40;
-    }
-
-    /**
-     * It calculates the score of a chance, which is the sum of the dices
-     * @param dices the dices array
-     * @returns {number} the score
-     */
-    function getChanceScore(dices){
-        var score = 0;
-        for(var index = 0 ; index <= NUMBER_OF_FACES ; ++ index){
-            score += dices[index] * index;
-        }
+    if(found)
         return score;
+    else
+        return 0;
+}
+
+/**
+ * Calculates the score of a full house
+ * If there is a pair and three dices of a kind, the score is 25 otherwise, 0
+ * @param dices the dices array
+ * @returns {number} the score
+ */
+function getFullHouseScore(dices){
+    var index = 1;
+    var pair_found = false;
+    var three_of_a_kind_found = false;
+    while(!(pair_found && three_of_a_kind_found) && index <= NUMBER_OF_FACES){
+        if(dices[index] === 2)
+            pair_found = true;
+        else if(dices[index] === 3)
+            three_of_a_kind_found = true;
+        index ++;
     }
 
-    function getYahtzeeScore(dices) {
-        var index = 1;
-        var score = 0;
-        var found = false;
-        while(!found && index <= NUMBER_OF_FACES){
-            score += dices[index] * index;
-            if(dices[index] === 5)
-                found = true;
-            else
-                index ++;
-        }
-        if(found)
-            return 50;
+    if(pair_found && three_of_a_kind_found)
+        return 25;
+    else
+        return 0;
+}
+
+/**
+ * Calculates a straight score
+ * If it detects a straight which its size is aimed_straight_length, then the score
+ * is 40 if it has a 5 dices straight, otherwise 30.
+ * @param aimed_straight_length aimed straight length
+ * @param dices the dices array
+ * @returns {number} the score
+ */
+function getStraightScore(aimed_straight_length, dices){
+    var max_straight_length = 0;
+    var straight_length = 0;
+
+    for(var index = 1 ; index <= NUMBER_OF_FACES ; ++ index){
+        // There is an element, so we start the straight otherwise we reset it
+        if(dices[index] !== 0)
+            straight_length ++;
         else
-            return 0;
+            straight_length = 0;
+
+        if(straight_length > max_straight_length){
+            max_straight_length = straight_length;
+        }
     }
 
+    if(max_straight_length < aimed_straight_length)
+        return 0;
+    else if(aimed_straight_length === 4)
+        return 30;
+    else
+        return 40;
+}
+
+/**
+ * It calculates the score of a chance, which is the sum of the dices
+ * @param dices the dices array
+ * @returns {number} the score
+ */
+function getChanceScore(dices){
+    var score = 0;
+    for(var index = 0 ; index <= NUMBER_OF_FACES ; ++ index){
+        score += dices[index] * index;
+    }
+    return score;
+}
+
+function getYahtzeeScore(dices) {
+    var index = 1;
+    var score = 0;
+    var found = false;
+    while(!found && index <= NUMBER_OF_FACES){
+        score += dices[index] * index;
+        if(dices[index] === 5)
+            found = true;
+        else
+            index ++;
+    }
+    if(found)
+        return 50;
+    else
+        return 0;
 }
